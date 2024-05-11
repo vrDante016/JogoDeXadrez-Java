7c23b615132b282380f6d436643d4423d4c2aa00
VrDante0116 <rafaelgermanojapa@gmail.com>
2024-05-11 10:46:26 -0300

    Tratamento de erros

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package Aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaDeXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
+import xadrez.XadrezExcecao;
 
 public class Programa {
 
@@ -14,16 +16,29 @@ public class Programa {
 		PartidaDeXadrez partida = new PartidaDeXadrez();
 		
 		while(true) {
-			UI.impressaoTabuleiro(partida.getPecas());
-			System.out.println("");
-			System.out.print("Origin");
-			PosicaoXadrez origin = UI.lerPosicaoXadrez(ler);
-			
-			System.out.println();
-			System.out.print("Alvo");
-			PosicaoXadrez alvo = UI.lerPosicaoXadrez(ler);
-			
-			PecaXadrez pecaCapturada = partida.pecaComida(origin, alvo);
+			try {
+				UI.clearScreen();
+				UI.impressaoTabuleiro(partida.getPecas());
+				System.out.println("");
+				System.out.print("Origin");
+				PosicaoXadrez origin = UI.lerPosicaoXadrez(ler);
+				
+				System.out.println();
+				System.out.print("Alvo");
+				PosicaoXadrez alvo = UI.lerPosicaoXadrez(ler);
+				
+				PecaXadrez pecaCapturada = partida.pecaComida(origin, alvo);
+				}
+				catch(XadrezExcecao e){
+					System.out.println(e.getMessage());
+					ler.next();
+					
+				}
+				catch(InputMismatchException e) {
+					System.out.println(e.getMessage());
+					ler.next();
+				}
+				
 		}
 	}
 
